7af65b6735827b1c1e4c6e672fef508754d0ffe0
DanielRSnell <hi@danielsnell.ninja>
2018-02-20 21:50:18 -0500

    first push

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gatsby + Wordpress.org Starter
+
+This repo was created to help Wordpress users who are interested in using Wordpress.org self hosted API with Gatsby.
---
+++ src/templates/post.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import Img from "gatsby-image"
+
+import PostIcons from "../components/PostIcons"
+
+import { rhythm } from "../utils/typography"
+
+class PostTemplate extends Component {
+  render() {
+    const post = this.props.data.wordpressPost
+
+    return (
+      <div>
+        <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+        <PostIcons node={post} css={{ marginBottom: rhythm(1 / 2) }} />
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    )
+  }
+}
+//<img src={post.image.sizes.thumbnail} />
+
+PostTemplate.propTypes = {
+  data: PropTypes.object.isRequired,
+  edges: PropTypes.array,
+}
+
+export default PostTemplate
+
+export const pageQuery = graphql`
+  query currentPostQuery($id: String!) {
+    wordpressPost(id: { eq: $id }) {
+      title
+      content
+    }
+  }
+`;
